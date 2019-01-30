03f2c5a9579e7ef5f36c2980672723160af16d32
ltndan <dan.deaconu@outlook.com>
2019-01-30 12:19:24 +0100

    added some html

--- ReactApp/app.tsx
+++ ReactApp/app.tsx
@@ -2,7 +2,17 @@ import * as React from 'react';
 import { render } from 'react-dom';
 
 const App = () => (
-  <div>i am alive</div>
+  <div style={{display: "flex"}}> {/*never ever do inline styles*/}
+  <div style={{flex: 1}}>
+<h2>Create user</h2>
+  </div>
+  <div style={{flex: 1}}>
+  <h2>Signin user</h2>
+  </div>
+  <div style={{flex: 1}}>
+  <h2>User api</h2>
+  </div>
+  </div>
 );
 
 render(<App />, document.getElementById('root'));
